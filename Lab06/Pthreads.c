26e10849662871c362ee07c0e1d560ac0b6f9dfe
Raphael Ferrari <64502502+Rapha-ferrari@users.noreply.github.com>
2022-09-12 16:53:47 -0300

    Update Pthreads.c

--- Lab06/Pthreads.c
+++ Lab06/Pthreads.c
@@ -19,3 +19,17 @@ node ME2[4][3] = {{{3, 1.0}}, {{2, 2.0}, {3, 3.0}}, {{1, 3.0}, {3, 2.0}}, {{1, 4
 float VETOR[4] = {1.0, 2.0, 3.0, 4.0};
 
 float MD[4][3] = {{0, 1, 2}, {3, 2, 1}, {2, 4, 2}, {3, 6, 2}};
+
+float EM_VECTORS[row];
+float EM_MATRIX[row][Dcolumns];
+float EM_EM2[row][Dcolumns2];
+
+int menu(){
+  system("clear");
+  int opt;
+  printf("                              MENU\n\n1 - MultiplicaÃ§Ã£o de uma matriz esparsa por um vetor denso\n2 - MultiplicaÃ§Ã£o de uma matriz esparsa por outra matriz esparsa\n3 - MultiplicaÃ§Ã£o de uma matriz esparsa por uma matriz densa \n4 - Sair\n\nopt: ");
+  scanf("%d", &opt);
+  return opt;
+
+}
+
