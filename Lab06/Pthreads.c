82565365755c2e677ef9ee3d5b1f32feb8189925
Raphael Ferrari <64502502+Rapha-ferrari@users.noreply.github.com>
2022-09-13 17:12:08 -0300

    Update Pthreads.c

--- Lab06/Pthreads.c
+++ Lab06/Pthreads.c
@@ -73,3 +73,84 @@ void *EM_X_EM(void *threadid){
 	pthread_exit(NULL);
 }
 
+void opt1(int*taskids, pthread_t *thread){
+	int;
+	system("limpar");
+			printf("1 - Multiplicação de uma matriz esparsa por um vetor denso\n");
+
+			for (i = 0; i < linha; i++){ 											
+				taskids[i]=i;
+				pthread_create(&thread[i], NULL, Vector_X_EM, (void*)&taskids[i]);	
+			}
+
+			for (i = 0; i < linha; i++){		
+				pthread_join(thread[i], NULL);
+				printf("%2f\n", EM_VECTORS[i]);
+			}
+	Retorna;
+}
+
+void opt2(int*taskids, pthread_t *thread){
+	int;
+	system("limpar");
+			printf("2 - Multiplicação de uma matriz esparsa por outra matriz esparsa\n");
+
+			for (i = 0; i < linha; i++){   										
+				taskids[i]=i;
+				pthread_create(&thread[i], NULL, EM_X_EM, (void*)&taskids[i]);	
+			}
+
+			for (i = 0; i < linha; i++){		
+				pthread_join(thread[i], NULL);
+				for (int j = 0; j < Dcolumns2; j++)
+					printf("%2f", EM_EM2[i][j]);
+				printf("\n");		
+			}
+	Retorna;
+}
+
+void opt3(int*taskids, pthread_t *thread){
+	int, j;
+	system("limpar");
+			printf("3 - Multiplicação de uma matriz esparsa por uma matriz densa\n");
+
+			for (i = 0; i < linha; i++){   										
+				taskids[i]=i;
+				pthread_create(&thread[i], NULL, EM_X_DM, (void*)&taskids[i]);	
+			}
+
+			for (i = 0; i < linha; i++){		
+				pthread_join(thread[i], NULL);
+				for (j = 0; j < Dcolunas2; j++)
+					printf("%2f", EM_MATRIX[i][j]);
+				printf("\n");		
+			}
+
+	Retorna;
+}
+
+int main(){
+	pthread_t thread[linha];
+	int *num, i, j, taskids[row], opt = 0, keep = 0;
+	opt = menu();
+	
+	while(opt != 4){
+
+		if(opt == 1) opt1(taskids, thread);
+
+		else if(opt == 2) opt2(taskids, thread);
+		
+		else if(opt == 3) opt3(taskids, thread);
+
+		printf("\n\nAperte 1 para continuar: ");
+			
+		scanf("%d", &keep);
+
+		opt = menu();
+
+	}
+	
+	pthread_exit(NULL);
+	
+	retornar 0;
+}
